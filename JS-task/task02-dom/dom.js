bef090986b4fb243a96c1b2c314d79560348418c
kkovacsics <kalman.kovacsics@gmail.com>
2021-04-18 17:59:24 +0200

    Feat: DOM task solved.

--- JS-task/task02-dom/dom.js
+++ JS-task/task02-dom/dom.js
@@ -1,5 +1,13 @@
+/* 
+ * megkeres az oldalon minden `example` osztályba tartozó elemet és két stílust 
+ * állít be rájuk JAVASCRIPT segítségével: háttérszín `red` és font stílus `italic`.
+ */
 function changeElements() {
-    // 
+    const elems = document.querySelectorAll('.example');
+    elems.forEach(elem => {
+        elem.style.backgroundColor = 'red';
+        elem.style.fontStyle = 'italic';
+    })
 }
 
 
